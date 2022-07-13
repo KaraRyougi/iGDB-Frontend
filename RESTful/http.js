8985456658dde05db3bee22834b4dec3303c92dd
KaraRyougi <shon.huang@gatech.edu>
2022-07-13 18:38:57 -0400

    sqlite query json result backend

--- RESTful/http.js
+++ RESTful/http.js
@@ -1,13 +1,58 @@
 import { Database } from "bun:sqlite";
 
-const db = Database.open("");
+const db = Database.open("./database", { readonly: true })
 
 export default {
   port: 3000,
-  fetch(request) {
-      
-    var asn_name = db.query("");
-    var asn_loc = db.query("");
-    asn_name.all();
-    },
-};
+  fetch(req) {
+    const url = new URL(req.url)
+    const asn = url.searchParams.get('asn')
+    const headers = new Headers({'content-type': 'application/json'})
+    var body = ''
+    
+//     if (asn is not a number) {
+//       return new Response({})
+//     }
+    
+    if (url.pathname == '/asn_org') {
+      const asn_org = db.query("SELECT * FROM 'asn_org' WHERE asn=$asn").all({
+        $asn: asn,
+      })
+      body = JSON.stringify(asn_org)
+    } else if (url.pathname == '/asn_geo') {
+      const asn_geo = db.query("SELECT * FROM 'asn_loc' WHERE asn=$asn").all({
+        $asn: asn,
+      })
+      body = JSON.stringify(asn_geo)
+//     } else {
+//       return new Response({})
+    }
+    return new Response(body, { headers, status: 200 })
+  }
+}
+
+// 
+// /asn_org?asn=46997
+// /asn_geo?asn=46997
+// 
+// 
+// {
+// "asn":
+// "data": [
+//   {
+//     "source": [
+//       "RIPEAtlas",
+//       "PeeringDB",
+//     ]
+//     "location": {
+//       "city":"",
+//       "state":"",
+//       "country_code":"",
+//       "coordinate": {
+//         "latitude":"",
+//         "longitude":""
+//       }
+//     },
+//     "last_update":"",
+//   },
+// ]}
--- docs/src/.vuepress/components/ASNMap.vue
+++ docs/src/.vuepress/components/ASNMap.vue
@@ -1,21 +1,16 @@
+<!-- TODOO:
+Input box format filter (pure number or AS[number])
+ -->
+
 <template>
 <div>
 <form @submit.prevent="onSubmit">
-  <v-select label="ASN" :v-model="selectedASN" multiple>
-    <template #search="{ attributes, events }">
-      <input
-        :required="!selectedASN"
-        class="vs__search"
-        v-bind="attributes"
-        v-on="events"
-      />
-    </template>
+  <v-select label="ASN" taggable multiple :dropdown-should-open="_=>{return false}" :options=[]>
+
   </v-select>
   <input type="submit" value="Plot"/>
 </form>
-
 <div ref="map" class="map">
-  <div id="tooltip"></div>
 </div>
 </div>
 </template>
